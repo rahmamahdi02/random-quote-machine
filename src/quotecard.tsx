9f77bb38da6720d9d79bfd5216f46ff403948ddf
Rahma <rahmamahdi.02@gmail.com>
2024-07-12 10:39:32 -0700

    update readme

--- src/quotecard.tsx
+++ src/quotecard.tsx
@@ -29,7 +29,7 @@ const Quote: React.FC<QuoteProps> = ({ quote, author, randomColor, transition, f
             transition,
           }}
         >
-        <span style={{ marginRight: '5px' }}>Tweet Now</span> {/* Adjust margin as needed */}
+        <span style={{ marginRight: '5px' }}>Tweet Now</span> 
           <FaTwitter color="white" />
         </a>
         <button
